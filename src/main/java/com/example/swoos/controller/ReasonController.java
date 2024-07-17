bf71922e6b7ab9d2096106c7287980c91ff91208
rajuhepl <nambi.-@hepl.com>
2024-07-17 09:47:08 +0530

    Bug fixed

--- src/main/java/com/example/swoos/controller/ReasonController.java
+++ src/main/java/com/example/swoos/controller/ReasonController.java
@@ -1,5 +1,6 @@
 package com.example.swoos.controller;
 
+import com.example.swoos.exception.CustomValidationException;
 import com.example.swoos.model.Reason;
 import com.example.swoos.service.ReasonService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +17,7 @@ public class ReasonController {
     }
 
     @PostMapping("/add")
-    public String addReason(@RequestParam int rowId, @RequestParam String reason) {
+    public String addReason(@RequestParam int rowId, @RequestParam String reason) throws CustomValidationException {
        return reasonService.addReason(rowId, reason);
     }
 
